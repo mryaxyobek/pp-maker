6d8b27359e0c35243cdebd557a2d7f7fc8f2fdb1
mryaxyobek <pubgn9642@gmail.com>
2023-08-10 21:49:41 +0500

    intial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+let canvas = document.getElementById("myCanvas");
+let context = canvas.getContext("2d");
+let input = document.getElementById("textInput");
+let loadButton = document.getElementById("loadButton");
+
+let canvasDownload = document.getElementById("canvasDownload");
+
+let image = new Image();
+image.src = "http://127.0.0.1:5501/images/cartoon_girl-www.pictures-for-profile-uz.netlify.app2.jpg";
+image.onload = function() {
+    context.drawImage(image, 0, 0, canvas.width, canvas.height);
+
+    context.font = "20px sans-serif";
+    context.fillStyle = "#4c4c4c";
+
+    loadButton.addEventListener('click', function() {
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.drawImage(image, 0, 0, canvas.width, canvas.height);
+        context.fillText(input.value, 100, 265); 
+
+        //  bliarc 
+        const pngDataUrl = canvas.toDataURL("image/png");
+        console.log(pngDataUrl);
+        canvasDownload.href = pngDataUrl;
+        console.log(pngDataUrl);
+    });
+};
