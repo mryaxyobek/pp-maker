083b297c0039f389c387c03fb0ae9d3cca7b0043
mryaxyobek <pubgn9642@gmail.com>
2023-08-22 14:25:23 +0500

    added main styles

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 const elDarkModeBtn = document.querySelector('.js-dark-mode-btn');
+const elPageCards = document.querySelectorAll('.page-card-child');
+const elPageCardsWrapper = document.querySelectorAll('.pages-cards-wrapper');
 
-elDarkModeBtn.addEventListener('click', function(){
-    elDarkModeBtn.disabled = true;  
-    setTimeout(function(){
+elDarkModeBtn.addEventListener('click', function () {
+    elDarkModeBtn.disabled = true;
+    setTimeout(function () {
         elDarkModeBtn.disabled = false;
         document.body.classList.toggle('dark-mode');
         elDarkModeBtn.childNodes[3].classList.toggle('shape-run');
         elDarkModeBtn.childNodes[3].classList.add('shape-intial-animation');
         elDarkModeBtn.childNodes[5].classList.remove('remove-intial-animation');
-    },500);
+    }, 500);
     elDarkModeBtn.childNodes[1].classList.toggle('sun-animation');
     elDarkModeBtn.childNodes[5].classList.toggle('sun-animation');
 });
