ddaa978930c456f19ddd99c0092bddafa3659be9
mryaxyobek <pubgn9642@gmail.com>
2023-08-22 08:28:16 +0500

    added dark mode

---
+++ script.js
@@ -0,0 +1,14 @@
+const elDarkModeBtn = document.querySelector('.js-dark-mode-btn');
+
+elDarkModeBtn.addEventListener('click', function(){
+    elDarkModeBtn.disabled = true;  
+    setTimeout(function(){
+        elDarkModeBtn.disabled = false;
+        document.body.classList.toggle('dark-mode');
+        elDarkModeBtn.childNodes[3].classList.toggle('shape-run');
+        elDarkModeBtn.childNodes[3].classList.add('shape-intial-animation');
+        elDarkModeBtn.childNodes[5].classList.remove('remove-intial-animation');
+    },500);
+    elDarkModeBtn.childNodes[1].classList.toggle('sun-animation');
+    elDarkModeBtn.childNodes[5].classList.toggle('sun-animation');
+});
