b46bb6cab7d874ba10b774826b81e91125966937
mryaxyobek <pubgn9642@gmail.com>
2023-08-27 17:14:18 +0500

    added new icons

--- listInformations.js
+++ listInformations.js
@@ -7,39 +7,14 @@ export const listInformations = [
     },
     {
         imgSrc: './images/toon-hijab-girl_(pp-maker.netlify.app-sayti-orqali-yuklab-olindi).jpg',
-        qualityImgSrc: './images/high-quality-toon-hijab-girl_(pp-maker.netlify.app-sayti-orqali-yuklab-olindi).jpg',
+        qualityImgSrc: './images/toon-hijab-girl_(pp-maker.netlify.app-sayti-orqali-yuklab-olindi).jpg',
         title: 'Hijobli qiz',
         imgAlt: 'toon girl'
     },
     {
-        imgSrc: './images/cartoon-girl(uploaded-profil-uchun-rasmlar.netlify.app)-compresse-image.jpg',
-        title: 'Multik qiz',
-        imgAlt: 'cartoon girl'
+        imgSrc: './images/toon-hijab-girl_(pp-maker.netlify.app-sayti-orqali-yuklab-olindi).jpg',
+        qualityImgSrc: './images/toon-hijab-girl_(pp-maker.netlify.app-sayti-orqali-yuklab-olindi).jpg',
+        title: 'Hijobli qiz',
+        imgAlt: 'toon girl'
     },
-    {
-        imgSrc: './images/cartoon-girl(uploaded-profil-uchun-rasmlar.netlify.app)-compresse-image.jpg',
-        title: 'Multik qiz',
-        imgAlt: 'cartoon girl'
-    },
-    {
-        imgSrc: './images/cartoon-girl(uploaded-profil-uchun-rasmlar.netlify.app)-compresse-image.jpg',
-        title: 'Multik qiz',
-        imgAlt: 'cartoon girl'
-    },
-    {
-        imgSrc: './images/cartoon-girl(uploaded-profil-uchun-rasmlar.netlify.app)-compresse-image.jpg',
-        title: 'Multik qiz',
-        imgAlt: 'cartoon girl'
-    },
-    {
-        imgSrc: './images/cartoon-girl(uploaded-profil-uchun-rasmlar.netlify.app)-compresse-image.jpg',
-        title: 'Multik qiz',
-        imgAlt: 'cartoon girl'
-    },
-    {
-        imgSrc: './images/cartoon-girl(uploaded-profil-uchun-rasmlar.netlify.app)-compresse-image.jpg',
-        title: 'Multik qiz',
-        imgAlt: 'cartoon girl'
-    },
-
 ];
