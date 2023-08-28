5aa0f95be223d110280aeee4c50fcc7e65fcbe9c
mryaxyobek <pubgn9642@gmail.com>
2023-08-28 09:01:21 +0500

    Added image download feature for users

--- script.js
+++ script.js
@@ -53,7 +53,7 @@ document.addEventListener('DOMContentLoaded', () => {
         document.body.classList.add('dark-mode');
         elDarkModeBtn.childNodes[3].classList.add('shape-run');
         elDarkModeBtn.childNodes[1].classList.add('sun-animation');
-        elDarkModeBtn.childNodes[5].classList.remove('remove-intial-animation','sun-animation');
+        elDarkModeBtn.childNodes[5].classList.remove('remove-intial-animation', 'sun-animation');
     }
 });
 
@@ -120,7 +120,7 @@ if (elPageCardNo1) {
             window.location.href = 'picturesforboys.html';
         }, 500);
     });
-}else{
+} else {
     console.log('Page Card link Child Not Found(121=>)');
 }
 
@@ -145,8 +145,8 @@ if (elImagesList) {
                     </svg>
             </button>
             <button quality-image=${e.qualityImgSrc} class="create-image-btn">
-                <span>Rasm tayyorlash</span>
-                <svg class="add-image-icon" width="22" height="22" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <span>Rasm tayyorlash</span>
+            <svg class="add-image-icon" width="22" height="22" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M12.5 3H7.8C6.11984 3 5.27976 3 4.63803 3.32698C4.07354 3.6146 3.6146 4.07354 3.32698 4.63803C3 5.27976 3 6.11984 3 7.8V16.2C3 17.8802 3 18.7202 3.32698 19.362C3.6146 19.9265 4.07354 20.3854 4.63803 20.673C5.27976 21 6.11984 21 7.8 21H17C17.93 21 18.395 21 18.7765 20.8978C19.8117 20.6204 20.6204 19.8117 20.8978 18.7765C21 18.395 21 17.93 21 17M19 8V2M16 5H22M10.5 8.5C10.5 9.60457 9.60457 10.5 8.5 10.5C7.39543 10.5 6.5 9.60457 6.5 8.5C6.5 7.39543 7.39543 6.5 8.5 6.5C9.60457 6.5 10.5 7.39543 10.5 8.5ZM14.99 11.9181L6.53115 19.608C6.05536 20.0406 5.81747 20.2568 5.79643 20.4442C5.77819 20.6066 5.84045 20.7676 5.96319 20.8755C6.10478 21 6.42628 21 7.06929 21H16.456C17.8951 21 18.6147 21 19.1799 20.7582C19.8894 20.4547 20.4547 19.8894 20.7582 19.1799C21 18.6147 21 17.8951 21 16.456C21 15.9717 21 15.7296 20.9471 15.5042C20.8805 15.2208 20.753 14.9554 20.5733 14.7264C20.4303 14.5442 20.2412 14.3929 19.8631 14.0905L17.0658 11.8527C16.6874 11.5499 16.4982 11.3985 16.2898 11.3451C16.1061 11.298 15.9129 11.3041 15.7325 11.3627C15.5279 11.4291 15.3486 11.5921 14.99 11.9181Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
                 </svg>
             </button>
@@ -173,7 +173,7 @@ createImageBtn.forEach(function (e) {
         elMainImage.onload = () => {
             setTimeout(function () {
                 elIMageDownloadAnm.classList.add('hidden');
-            },500);
+            }, 500);
         };
         // 
         elCreationIMageWrapper.classList.remove('hidden-2');
@@ -185,55 +185,43 @@ createImageBtn.forEach(function (e) {
                 elCreationImageDarkFilter.classList.add('hidden');
             } else { console.log('image creation place not found'); };
         });
-        elCreationImageDarkFilter.addEventListener('click', function(){
+        elCreationImageDarkFilter.addEventListener('click', function () {
             elCreationIMageWrapper.classList.add('hidden-2');
-                elCreationImageDarkFilter.classList.add('hidden');
-            });
+            elCreationImageDarkFilter.classList.add('hidden');
+        });
         //     
         elCloseIMageCreationBtn.addEventListener('click', function () {
             elCreationIMageWrapper.classList.add('hidden-2');
             elCreationImageDarkFilter.classList.add('hidden');
         });
+        // draw image
+        let elNameInput = document.querySelector(".name-input");
+        // let elAddNameBtn = document.querySelector(".name-adder-btn");
+        let elDownLoadBtn = document.querySelector(".image-downloader-link");
+
+        let canvas = document.createElement('canvas');
+        let context = canvas.getContext("2d");
+        canvas.width = '1500';
+        canvas.height = '1500';
+
+        let canvasImage = new Image();
+        canvasImage.src = `${e.getAttribute('quality-image')}`
+        canvasImage.onload = function () {
+            context.drawImage(canvasImage, 0, 0, canvas.width, canvas.height);
+            context.font = "100px sans-serif";
+        };
+        let elImageForm = document.querySelector('.create-image-form');
+        elImageForm.addEventListener('submit', function (e) {
+            e.preventDefault();
+
+            let ming = 1500 - context.measureText(elNameInput.value).width;
+            let ming2 = ming - ming / 2;
+            context.clearRect(0, 0, 1500, 1500);
+            context.drawImage(canvasImage, 0, 0, 1500, 1500);
+            context.fillText(elNameInput.value, ming2, 1265);
+
+            const canvasDataUrl = canvas.toDataURL("image/png");
+            elDownLoadBtn.href = `${canvasDataUrl}`
+        });
     });
 });
-
-
-// // draw image
-
-// let input = document.querySelector(".textInput");
-// let elAddNameBtn = document.querySelector(".js-add-name-btn");
-// let elDownLoadBtn = document.querySelector(".js-download-btn");
-// let elEditBtn = document.querySelector(".edit-btn");
-// let elButtonsWrapperSecondChild = document.querySelector(".buttons-wrapper-second-child");
-// let elButtonsWrapperFirstChild = document.querySelector(".buttons-wrapper-first-child");
-
-// let canvas = document.createElement('canvas');
-// let context = canvas.getContext("2d");
-// canvas.style.fontFamily = 'akhilona';
-// canvas.width = '1000'
-// canvas.height = '1000'
-
-// let image = new Image();
-// image.src = "./images/cartoon-girl(uploaded-profil-uchun-rasmlar.netlify.app)-image.jpg";
-// image.onload = function () {
-//     let elform = document.querySelector('.form');
-    
-//     context.fontFamily = 'hadnich'
-//     context.drawImage(image, 0, 0, 1000, 1000);
-//     context.font = "100px hadnich";
-
-//     elform.addEventListener('submit', function (e) {
-//         e.preventDefault();
-
-//         let ming = 1000 - context.measureText(input.value).width;
-//         let ming2 = ming - ming / 2;
-//         context.clearRect(0, 0, 1000, 1000);
-//         context.drawImage(image, 0, 0, 1000, 1000);
-//         context.fillText(input.value, ming2, 880);
-
-//         const pngDataUrl = canvas.toDataURL("image/png");
-//         elDownLoadBtn.href = pngDataUrl;
-//         elButtonsWrapperFirstChild.classList.add('right-100')
-//         elButtonsWrapperSecondChild.classList.remove('left-110')
-//     });
-// };
