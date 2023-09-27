c2ac8bc4d45b34c6da4dee60bdc1bda3553f8e39
mryaxyobek <pubgn9642@gmail.com>
2023-09-27 16:28:02 +0500

    unnecessary images have been deleted

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
+import Logo from '../assets/images/images/'
 const Header = () => {
     return (
         <header className="header py-8 sticky w-full top-0 right-0 left-0">
             <div className="container">
                 {/* logo  */}
-
+                <Link to='/'>
+                    <img src={Logo} alt="profile picture maker logo" />
+                </Link>
             </div>
         </header>
     )
