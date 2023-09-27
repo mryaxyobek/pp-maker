a97c744a7617227db0d6e0763ab0c88d48ef4378
mryaxyobek <pubgn9642@gmail.com>
2023-09-27 16:24:02 +0500

    added global styles

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header className="header py-8 sticky w-full top-0 right-0 left-0">
+            <div className="container">
+                {/* logo  */}
+
+            </div>
+        </header>
+    )
+}
+
+export default Header
--- src/layouts/MainRoot.jsx
+++ src/layouts/MainRoot.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Top from '../components/Top'
 // import Header from '../components/Header'
 
 const MainRoot = () => {
   return (
     <div className='flex flex-col min-h-screen'>
+      <Top />
       {/* <Header /> */}
       <main className='grow'>
         <Outlet />
