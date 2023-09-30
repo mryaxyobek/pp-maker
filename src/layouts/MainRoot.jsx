dc9b4404ab7da632bc93d4416832a70204179aa8
mryaxyobek <pubgn9642@gmail.com>
2023-09-30 23:03:34 +0500

    added top section

--- src/layouts/MainRoot.jsx
+++ src/layouts/MainRoot.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Header from '../components/Header'
+import Top from '../components/Top'
 
 const MainRoot = () => {
   return (
     <div className='flex flex-col min-h-screen'>
+      <Top />
       <Header />
       <main className='grow'>
         <Outlet />
